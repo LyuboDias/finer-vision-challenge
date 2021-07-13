bf54062655c32ebcb01839ccd2c370bdca48b214
Lyubomir Dias <lyubomirdias@gmail.com>
2021-07-13 10:48:06 +0100

    add express

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
 
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  port: 3306,
+  database: "mydb",
+});
+
 app.get("/", (req, res) => {
-  res.send("samooo levski");
+  const sqlInsert =
+    "INSERT INTO `crud_app` (`firstName`, `lastName, email`, `telephone, gender`, `dob`) VALUES ('Lyubo', 'Dias', 'lyubomir@gmil.com', '0800828828', 'male', '01.01.2011')";
+  db.query(sqlInsert, (err, result) => {
+    res.send("Zdrasti Lyuboooo vkarahme noviq user");
+  });
 });
 
 app.listen(3001, () => {
